61898bb7b78657899219f4835d25a837c194fd5d
Daniel Hanold <dhanold@dnainfo.com>
2016-07-29 17:43:40 -0400

    Begin with configuration form

--- modules/custom/push_notifications/src/Form/PushNotificationsConfigForm.php
+++ modules/custom/push_notifications/src/Form/PushNotificationsConfigForm.php
@@ -32,7 +32,25 @@ class PushNotificationsConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    // Form constructor.
+    $form = parent::buildForm($form, $form_state);
+
+    // Get config.
     $config = $this->config('push_notifications.PushNotificationsConfig');
+
+    $configuration_apns_replacements = array(
+      '@link' => 'http://blog.boxedice.com/2009/07/10/how-to-build-an-apple-push-notification-provider-server-tutorial/',
+      ':cert_name_development' => push_notifications_get_certificate_name('development'),
+      ':cert_name_production' => push_notifications_get_certificate_name('production'),
+    );
+
+    $form['apns'] = array(
+      '#type' => 'fieldset',
+      '#title' => $this->t('Apple Push Notifications'),
+      '#description' => $this->t('Configure Push Notifications for Apple\'s Push Notification Server. Select your environment. Both environments require the proper certificates in the \'certificates\' folder of this module.<br />The filename for the development certificate should be \':cert_name_development\', the production certificate should be \':cert_name_production\'. See <a href="@link" target="_blank">this link</a> for instructions on creating certificates.', $configuration_apns_replacements),
+    );
+
+
     return parent::buildForm($form, $form_state);
   }
 
