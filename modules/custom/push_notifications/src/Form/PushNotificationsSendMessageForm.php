4448656e0540a7be08539d6aa4e5d6a8eef652c3
Daniel Hanold <danny@danielhanold.com>
2016-07-30 11:43:23 -0400

    Create form for sending out push notifications

--- modules/custom/push_notifications/src/Form/PushNotificationsSendMessageForm.php
+++ modules/custom/push_notifications/src/Form/PushNotificationsSendMessageForm.php
@@ -12,7 +12,6 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class PushNotificationsSendMessageForm extends FormBase {
 
-
   /**
    * {@inheritdoc}
    */
@@ -25,17 +24,46 @@ class PushNotificationsSendMessageForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    $form['instructions'] = array(
+      '#type' => 'item',
+      '#markup' => $this->t('Compose the elements of your push notification message.'),
+    );
+
+
+    $form['message'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Push Message'),
+      '#description' => $this->t('Compose the message to send out (@limit characters max.)', array(
+        '@limit' => PUSH_NOTIFICATIONS_APNS_PAYLOAD_SIZE_LIMIT,
+      )),
+      '#required' => TRUE,
+      '#size' => 128,
+      '#maxlength' => PUSH_NOTIFICATIONS_APNS_PAYLOAD_SIZE_LIMIT,
+    );
+
+    // Only show Android option if GCM Api Key is available..
+    $recipients_options = array('ios' => t('iOS (Apple Push Notifications)'));
+    if (!empty(\Drupal::config('push_notifications.gcm')->get('api_key'))) {
+      $recipients_options['android'] = t('Android (Google Cloud Messaging)');
+    }
+    $form['recipients'] = array(
+      '#type' => 'checkboxes',
+      '#title' => t('Target Networks'),
+      '#description' => t('Select the networks you want to reach with this message.'),
+      '#options' => $recipients_options,
+    );
+
     $form['submit'] = array(
-        '#type' => 'submit',
-        '#value' => t('Submit'),
+      '#type' => 'submit',
+      '#value' => t('Send Push Notification'),
     );
 
     return $form;
   }
 
   /**
-    * {@inheritdoc}
-    */
+   * {@inheritdoc}
+   */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
   }
@@ -46,7 +74,7 @@ class PushNotificationsSendMessageForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Display result.
     foreach ($form_state->getValues() as $key => $value) {
-        drupal_set_message($key . ': ' . $value);
+      drupal_set_message($key . ': ' . $value);
     }
 
   }
